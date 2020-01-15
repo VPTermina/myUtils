332d980c7f1587102541267dd08b8084669210bf
VPTermina <42435941+VPTermina@users.noreply.github.com>
2020-01-15 15:15:13 +0100

    version 10

--- testGitHub/src/testGitHub/myGitTest.java
+++ testGitHub/src/testGitHub/myGitTest.java
@@ -5,7 +5,7 @@ public class myGitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Programm angelegt 9");
+		System.out.println("Programm angelegt 10");
 
 		
 		 
