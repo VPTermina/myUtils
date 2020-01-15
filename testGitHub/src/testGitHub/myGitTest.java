011700277a91926ff18adca144763c7c853f0505
VPTermina <42435941+VPTermina@users.noreply.github.com>
2020-01-15 10:48:37 +0100

    First commit

---
+++ testGitHub/src/testGitHub/myGitTest.java
@@ -0,0 +1,13 @@
+package testGitHub;
+
+public class myGitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+		System.out.println("Programm angelegt 1");
+		
+		
+	}
+
+}
