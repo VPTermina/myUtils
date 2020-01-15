6dfae1e15da74a83caa3a6cc1223d4f08e49a21d
u991712 <u991712@WOL-NB-B849.nucleus.atom.ads>
2020-01-15 13:28:42 +0100

    commit von PC2

--- testGitHub/src/testGitHub/myGitTest.java
+++ testGitHub/src/testGitHub/myGitTest.java
@@ -5,7 +5,7 @@ public class myGitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    
-		System.out.println("Programm angelegt 1");
+		System.out.println("Programm angelegt 2");
 		
 		
 	}
